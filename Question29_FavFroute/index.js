6f10e738afc0f544bac198e476bde715b2a54f58
Furqan-Nizamani <165314572+Furqan-Nizamani@users.noreply.github.com>
2024-04-21 21:59:14 +0500

    Add files via upload

---
+++ Question29_FavFroute/index.js
@@ -0,0 +1,29 @@
+"use strict";
+/*
+
+Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+
+• Make a array of your three favorite fruits and call it favorite_fruits.
+
+• Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+
+*/
+let favorite_fruits = ["ceive", "mango", "apple"];
+if (favorite_fruits.includes("ceive")) {
+    console.log("You really like ceive");
+}
+if (favorite_fruits.includes("mango")) {
+    console.log("You really like mango");
+}
+if (favorite_fruits.includes("apple")) {
+    console.log("You really like apple");
+}
+if (favorite_fruits.includes("strowbary")) {
+    console.log("You really like strowbary");
+}
+if (favorite_fruits.includes("banana")) {
+    console.log("You really like banana");
+}
+if (favorite_fruits.includes("paineApple")) {
+    console.log("You really like paineApple");
+}
