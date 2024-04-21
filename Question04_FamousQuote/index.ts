6ab5952e61a650b0c8bda6aa2030f2076cc7fca4
Furqan-Nizamani <165314572+Furqan-Nizamani@users.noreply.github.com>
2024-04-21 18:48:09 +0500

    Add files via upload

---
+++ Question04_FamousQuote/index.ts
@@ -0,0 +1,17 @@
+/*
+
+Famous Quote: Find a quote from a famous person you admire. 
+Print the quote and the name of its author. 
+Your output should look something like the following, 
+including the quotation marks:
+
+Albert Einstein once said, 
+“A person who never made a mistake never tried anything new.”
+
+*/
+
+let autherName: string = "Albert Einstein"
+let quote: string = "A person who never made a mistake never tried anything new."
+let sentance: string = `${autherName} once said, ${quote}`
+
+console.log(sentance);
