4f56d42c5e88807e8358a2aeba41d4348b1d2d7f
Furqan-Nizamani <165314572+Furqan-Nizamani@users.noreply.github.com>
2024-04-21 19:05:16 +0500

    Add files via upload

---
+++ Question07\08/index.js
@@ -0,0 +1,23 @@
+"use strict";
+/*
+
+Number Eight: Write addition, subtraction, multiplication,
+and division operations that each result in the number 8.
+Be sure to enclose your operations in print statements to see the results.
+
+You should create four lines that look like this:
+
+console.log(5 + 3)
+
+Your output should simply be four lines with the number 8 appearing once on each line.
+
+*/
+// Note: QUESTION NUMBER 7 AND 8 ARE CONNECTED WITH EACH OTHER THATS WHY IN THE SAME DIRACTRY.
+// Addition
+console.log(`Addition: ${5 + 3}`);
+// subtrction
+console.log(`Subtraction: ${16 - 8}`);
+//Multiplication
+console.log(`Multiplication: ${4 * 2}`);
+// Division
+console.log(`Division: ${16 / 2}`);
