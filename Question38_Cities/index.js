98b9c8eff14ffdf98701cc44c8ad5f484d4ab95c
Furqan-Nizamani <165314572+Furqan-Nizamani@users.noreply.github.com>
2024-04-21 22:20:00 +0500

    Add files via upload

---
+++ Question38_Cities/index.js
@@ -0,0 +1,14 @@
+"use strict";
+/*
+
+Cities: Write a function called describe_city() that accepts the name of a city and its country.
+The function should print a simple sentence, such as Karachi is in Pakistan. Give the parameter for the country a default value.
+Call your function for three different cities, at least one of which is not in the default country.
+
+*/
+function describe_city(city, country = "unknown") {
+    console.log(`the ${city} is in the ${country}`);
+}
+describe_city("HyderAbad", "Pakistan");
+describe_city("Dehli", "India");
+console.log("NewYourk");
