ae5925d7a4bd5ef93e8a5396fed065d5fc5fa43b
Furqan-Nizamani <165314572+Furqan-Nizamani@users.noreply.github.com>
2024-04-21 22:16:52 +0500

    Add files via upload

---
+++ Question36_T-Shirts/index.js
@@ -0,0 +1,14 @@
+"use strict";
+/*
+
+T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt.
+The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+
+*/
+function make_shirt() {
+    let size = "Large";
+    let text = "I love coding";
+    console.log(`the size of shirt is ${size}`);
+    console.log(`the brand of shirt is${text}`);
+}
+make_shirt();
---
+++ Question36_T-Shirts/index.ts
@@ -0,0 +1,17 @@
+/*
+
+T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. 
+The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+
+*/
+
+function make_shirt(){
+    let size: number = 19
+    let text: string = "I love coding"
+    console.log(`the size of shirt is ${size}`);
+    console.log(`the brand of shirt is${text}`);
+    
+    
+}
+
+make_shirt()
