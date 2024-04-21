e0c690621a54f1d0587666216f7b88ffe2a18351
Furqan-Nizamani <165314572+Furqan-Nizamani@users.noreply.github.com>
2024-04-21 19:10:52 +0500

    Add files via upload

---
+++ Question11_NameArray/index.js
@@ -0,0 +1,17 @@
+"use strict";
+/*
+
+Names: Store the names of a few of your friends in a array called names.
+Print each person’s name by accessing each element in the list, one at a time.
+
+*/
+let names = ["AbdulRaqueb", "Furqan", "AjmalKhan", "Samad"];
+// 1st condition.
+for (let i = 0; i < names.length; i++) {
+    console.log(`Hello Dear ${names[i]}, how are you?`);
+}
+// 2nd condition.
+console.log(`Hello Dear ${names[0]}, How are you?`);
+console.log(`Hello Dear ${names[1]}, How are you?`);
+console.log(`Hello Dear ${names[2]}, How are you?`);
+console.log(`Hello Dear ${names[3]}, How are you?`);
---
+++ Question11_NameArray/index.ts
@@ -0,0 +1,15 @@
+/*
+
+Names: Store the names of a few of your friends in a array called names. 
+Print each person’s name by accessing each element in the list, one at a time.
+
+*/
+
+let names: string[] = ["AbdulRaqueb", "Furqan", "AjmalKhan", "Samad"]
+// to access arry when can use loop but in the assingment
+// it is said that access each element one at a time 
+// so i think sir is just want jo check that do we know array index rules
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
