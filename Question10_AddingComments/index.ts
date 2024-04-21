b6dcf43d61f0f89b18616d12cac76d2537042f99
Furqan-Nizamani <165314572+Furqan-Nizamani@users.noreply.github.com>
2024-04-21 19:09:03 +0500

    Add files via upload

---
+++ Question10_AddingComments/index.ts
@@ -0,0 +1,21 @@
+/*
+ 
+Adding Comments: Choose two of the programs you’ve written, 
+and add at least one comment to each. 
+If you don’t have anything specific to write because your programs are too simple at this point, 
+just add your name and the current date at the top of each program file. 
+Then write one sentence describing what the program does.
+
+*/
+
+// Here I am Defining a constant variable named fomouse_person and assign the value "Heallen Kaller"
+//ham ne ik veriable banaya he or us me jiss ka quote he us ka nam likha h
+const famous_person = "Heallen Kaller";
+
+// Create a template literal string using backticks (``) and the ${} syntax for string interpolation.
+// The famous_person variable is inserted into the template literal.
+// The resulting message will be assigned to the constant variable named message.
+const message = `${famous_person} once said, "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart."`;
+
+// Output the message to the console
+console.log(message);
