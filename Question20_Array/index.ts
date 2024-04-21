1a2e97eaed15afcc74cbce53695a8d35da9efad5
Furqan-Nizamani <165314572+Furqan-Nizamani@users.noreply.github.com>
2024-04-21 19:37:40 +0500

    Add files via upload

---
+++ Question20_Array/index.ts
@@ -0,0 +1,15 @@
+/*
+
+Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+*/
+
+let names: string[] = ["Furqan", "Ali", "Samad", "Abdul Raqueb"]
+
+console.log("List of names: ");
+
+names.forEach(name => {
+    console.log(name);
+    
+})
+
