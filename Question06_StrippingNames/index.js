5742f7cfdd1b60ac29f5ca6c49ab4068c1c2d4b4
Furqan-Nizamani <165314572+Furqan-Nizamani@users.noreply.github.com>
2024-04-21 19:02:02 +0500

    Add files via upload

---
+++ Question06_StrippingNames/index.js
@@ -0,0 +1,24 @@
+"use strict";
+/*
+
+Stripping Names: Store a person’s name,
+and include some whitespace characters at the beginning and end of the name.
+Make sure you use each character combination, "\t" and "\n", at least once.
+Print the name once, so the whitespace around the name is displayed.
+Then print the name after striping the white spaces.
+
+*/
+// step 1 person name with white space.
+let person_name = "\tFurqan\n";
+console.log(person_name);
+// step 2 person name without white space.
+let personName = "\tFurqan\n";
+console.log(personName.trim());
+// 2nd condition
+let nameWithWhiteSpace = "\tFurqan\n";
+// printing name with white space.
+console.log(`Name with white space "${nameWithWhiteSpace}"`);
+// name striping.
+let stripingName = nameWithWhiteSpace.trim();
+// printing name without white space.
+console.log(`Name without white space "${stripingName}"`);
