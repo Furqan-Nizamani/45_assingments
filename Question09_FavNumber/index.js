1c591d4915e4191ee7d382a1e613d79b99822b92
Furqan-Nizamani <165314572+Furqan-Nizamani@users.noreply.github.com>
2024-04-21 19:06:39 +0500

    Add files via upload

---
+++ Question09_FavNumber/index.js
@@ -0,0 +1,14 @@
+"use strict";
+/*
+
+Favorite Number: Store your favorite number in a variable.
+Then, using that variable, create a message that reveals your favorite number.
+Print that message.
+
+*/
+// Store your favorite number in a variable
+let favoriteNumber = 7;
+//create a message that reveals your favorite number.
+let message = `my favorite number is ${favoriteNumber}.`;
+// Print that message.
+console.log(message);
---
+++ Question09_FavNumber/index.ts
@@ -0,0 +1,20 @@
+/*
+
+Favorite Number: Store your favorite number in a variable. 
+Then, using that variable, create a message that reveals your favorite number. 
+Print that message.
+
+*/
+
+// Store your favorite number in a variable
+
+let favoriteNumber: number = 7
+
+//create a message that reveals your favorite number.
+
+let message: string = `my favorite number is ${favoriteNumber}.`
+
+// Print that message.
+
+console.log(message);
+
