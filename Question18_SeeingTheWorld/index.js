9364613062ea4649d0a5f997754d133e8c40403f
Furqan-Nizamani <165314572+Furqan-Nizamani@users.noreply.github.com>
2024-04-21 19:33:11 +0500

    Add files via upload

---
+++ Question18_SeeingTheWorld/index.js
@@ -0,0 +1,50 @@
+"use strict";
+/*
+
+Seeing the World: Think of at least five places in the world you’d like to visit.
+• Store the locations in a array. Make sure the array is not in alphabetical order.
+
+• Print your array in its original order.
+
+• Print your array in alphabetical order without modifying the actual list.
+
+• Show that your array is still in its original order by printing it.
+
+• Print your array in reverse alphabetical order without changing the order of the original list.
+
+• Show that your array is still in its original order by printing it again.
+
+• Reverse the order of your list. Print the array to show that its order has changed.
+
+• Reverse the order of your list again. Print the list to show it’s back to its original order.
+
+• Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+
+• Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.
+
+*/
+let contryes = ["USA", "UK", "Italy", "China"];
+// Print your array in its original order.
+console.log(contryes);
+// Print your array in alphabetical order without modifying the actual list.
+console.log([...contryes].sort());
+// Show that your array is still in its original order by printing it.
+console.log(contryes);
+// Print your array in reverse alphabetical order without changing the order of the original list.
+console.log([...contryes].sort().reverse());
+// Show that your array is still in its original order by printing it again.
+console.log(contryes);
+// Reverse the order of your list. Print the array to show that its order has changed.
+console.log(contryes.reverse());
+// Print the array to show that its order has changed.
+console.log(contryes);
+// Reverse the order of your list again. Print the list to show it’s back to its original order.
+console.log(contryes.reverse());
+// Print the list to show it’s back to its original order.
+console.log(contryes);
+// Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+console.log(contryes.sort());
+// Print the array to show that its order has been changed.
+console.log(contryes.sort());
+// Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.
+console.log(contryes.reverse().sort());
