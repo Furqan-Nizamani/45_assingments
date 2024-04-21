1ba7438002668d5b0faa7b7ab77b7164925ea073
Furqan-Nizamani <165314572+Furqan-Nizamani@users.noreply.github.com>
2024-04-21 18:41:42 +0500

    Add files via upload

---
+++ Question02_message/index.js
@@ -0,0 +1,20 @@
+"use strict";
+/*
+
+Personal Message: Store a person’s name in a variable,
+and print a message to that person. Your message should be simple,
+such as, “Hello Eric, would you like to learn some Python today?”
+
+*/
+// condition 1
+let personName = "Furqan";
+console.log(`Hello ${personName}, would you like to learn some python today?`);
+// condition 2
+let person_name = "Furqan";
+let person_message = "would you like to learn some python today?";
+console.log(`Hello ${personName}, ${person_message}`);
+// condition 3
+let PersonName = "Furqan";
+let PersonMessage = `Hello ${PersonName}, would you like to learn some python today?`;
+console.log(PersonMessage);
+// This Question has been complete with 3 condition. you can chose whatever condition you want to chose
